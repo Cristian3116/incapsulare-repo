190d79da09e9c27c0f0e3e575e18ddb12ef0a2cd
Mija Cristian <mija.cristian3101@gmail.com>
2025-10-08 19:41:43 +0300

    Add EncapsulationRun.java

---
+++ src/EncapsulationRun.java
@@ -0,0 +1,12 @@
+public class EncapsulationRun {
+
+    public static void main(String[] args) {
+        Encapsulation encap = new Encapsulation();
+        encap.setName("James");
+        encap.setAge(20);
+        encap.setIdNum("12343ms");
+
+        System.out.println("Name " + encap.getName() + "Age " + encap.getAge() + "IdNum" + encap.getIdNum());
+
+    }
+}

f677c4080e156c09f1c6ade987881443cf9c6cf5
Mija Cristian <mija.cristian3101@gmail.com>
2025-10-08 19:16:54 +0300

    Add Class Circle

---
+++ src/Circle.java
@@ -0,0 +1,29 @@
+public class Circle {
+
+    //Variables with privat access
+    private double radius;
+    private String color;
+
+    //Costructors supercharged
+    public Circle(){
+        radius = 1.0;
+        color = "red";
+    }
+    public Circle(double r){
+        radius = r;
+        color = "red";
+    }
+    public Circle(double r,String c){
+        radius = r;
+        color = c;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public String getColor(){
+        return color;
+    }
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+}
